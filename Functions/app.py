7f307586e403816c5c6f9c926504cebda57f3f77
francis556 <vikrammehra244@gmail.com>
2022-01-26 13:49:11 +0200

    function practice

--- Functions/app.py
+++ Functions/app.py
@@ -8,3 +8,14 @@ result2 = firstnlast('Joe', 'Soap')
 print(result2)
 
 
+def print_lyrics():
+    print("I'm a lumberjack, and am okey")
+    print("I sleep all night and work all day")
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+repeat_lyrics()
