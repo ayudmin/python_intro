2980250042561439723fdda54489ad7e71dabeb5
francis556 <vikrammehra244@gmail.com>
2022-02-06 22:40:12 +0200

    variable values get replaced with new assigning

---
+++ Variables_n_simple_Data_types/variables.py
@@ -0,0 +1,18 @@
+print('Hello world Ayume!')  # syntax highlighting differentiates code and from basic words
+
+""" 
+    using variables in code the word message is a variable used to store the text my variable
+    and the print statement is a python function  used to print onscreen or out what is inthe
+    variable message ( my variable )
+"""
+
+message = 'my variable'
+print(message)
+
+"""
+    Modified value for variable ie variables act like containers meaning there current value or
+    content will get replaced with a new assigning
+"""
+
+message = 'Hello Python variables'
+print(message)
