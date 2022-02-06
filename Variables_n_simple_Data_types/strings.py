8f19f1e2928cfdadeeea2c7aa3947966225f87cd
francis556 <vikrammehra244@gmail.com>
2022-02-06 23:17:39 +0200

    strings in python

---
+++ Variables_n_simple_Data_types/strings.py
@@ -0,0 +1,9 @@
+"""
+    A string is a datatype in python ie anything enclosed in a quote ( double or single )
+"""
+
+_first_string = 'This is a string.'
+
+my_2string = " This is also another string @@#$$."
+
+print(_first_string, my_2string)
