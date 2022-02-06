d6f28fef1f4889c1a52c298dd6138ead7ae5d8be
francis556 <vikrammehra244@gmail.com>
2022-02-06 23:02:43 +0200

    variables are labels ie naming content

--- Variables_n_simple_Data_types/variables.py
+++ Variables_n_simple_Data_types/variables.py
@@ -2,7 +2,7 @@ print('Hello world Ayume!')  # syntax highlighting differentiates code and from 
 
 """ 
     using variables in code the word message is a variable used to store the text my variable
-    and the print statement is a python function  used to print onscreen or out what is inthe
+    and the print statement is a python function  used to print onscreen or out what is in the
     variable message ( my variable )
 """
 
@@ -31,3 +31,9 @@ my2002_birthday = '2022'
 _2021 = 'Not good yr'
 
 print(_my_name, name_my, my2002_birthday, _2021)
+
+"""   
+    *****
+    Consider variables as labels to variables ie names used to label content or something
+    *****
+"""
