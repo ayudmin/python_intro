6f154be8f81bcbd812bf489223408653b6101f69
francis556 <vikrammehra244@gmail.com>
2022-02-13 21:06:15 +0200

    defining a dictionary

---
+++ Dictionaries/dic.py
@@ -0,0 +1,14 @@
+""" Basic Python Dictionaries """
+
+alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+thisdic = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+
+print(thisdic['model'])
