8d086da2769654950532c411b8e62c6caceda854
francis556 <vikrammehra244@gmail.com>
2023-07-07 16:48:14 +0200

    info: striping off white spaces

---
+++ Data_types/white_space.py
@@ -0,0 +1,17 @@
+favorite_language = 'Python '
+
+print(favorite_language)
+print(len(favorite_language))
+
+no_spaces = favorite_language.rstrip()
+
+print(no_spaces)
+print(len(no_spaces))
+
+
+second_favorite = ' javascript'
+
+print(second_favorite)
+second_favorite = second_favorite.lstrip()
+
+print(second_favorite)
