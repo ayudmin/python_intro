e925616ec243d5caf1fc680e647b1c55e47ca05f
francis556 <vikrammehra244@gmail.com>
2023-07-08 17:00:00 +0200

    info: syntax error

---
+++ Data_types/apostrophe.py
@@ -0,0 +1,9 @@
+message = "One of python's strength is it's diverse community"
+
+print(message)
+
+""" syntax errors """
+
+# _message = 'one of python's strength is it's diverse community'
+
+print(_message)
