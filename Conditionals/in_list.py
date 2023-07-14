9250fced76c3884939a71b9b34e4f802fff28020
francis556 <vikrammehra244@gmail.com>
2023-07-14 18:16:08 +0200

    info in and not

---
+++ Conditionals/in_list.py
@@ -0,0 +1,4 @@
+family = ['ayume', 'merci', 'gladys']
+
+if 'ayume' in family:
+    print('Yap! ayume is available')
---
+++ Conditionals/not_in_list.py
@@ -0,0 +1,4 @@
+family = ['ayume', 'merci', 'gladys']
+
+if 'bdvdjbvd' not in family:
+    print('Definately not a family member')
