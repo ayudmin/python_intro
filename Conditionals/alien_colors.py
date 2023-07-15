c619a0d95d291ca62273370b509a7cb0b556c944
francis556 <vikrammehra244@gmail.com>
2023-07-15 14:15:46 +0200

    info: alien colors

---
+++ Conditionals/alien_colors.py
@@ -0,0 +1,22 @@
+alien_color = 'red' # yellow or red
+
+if alien_color == 'green':
+    print('Player just earned 5 points!')
+
+if alien_color == 'yellow':
+    print('Player just earned 5 points!')
+
+if alien_color == 'green':
+    print('Player just earned 5 points for shooting the alien!')
+
+if alien_color != 'green':
+    print('Player just earned 10 points!')
+
+
+if alien_color == 'green':
+    print('Player just earned 5 points for shooting the alien!')
+elif alien_color == 'yello':
+    print('Player just earned 10 points!')
+elif alien_color == 'red':
+    print('Player just earned 15 points!')
+
