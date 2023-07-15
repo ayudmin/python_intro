71774ad08afe3b054fee187c449f4039fa1a205a
francis556 <vikrammehra244@gmail.com>
2023-07-15 14:06:23 +0200

    info: block of if statements

--- Conditionals/topping.py
+++ Conditionals/topping.py
@@ -2,3 +2,18 @@ requested_topping = 'mushrooms'
 
 if requested_topping != 'anchovies':
     print('Hold the anchovies!')
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+print('**********************************')
+print('\n')
+
+if 'mushrooms' in requested_toppings:
+    print('Adding mushrooms.')
+if 'pepporina' in requested_toppings:
+    print('Adding pepporina.')
+if 'extra cheese' in requested_toppings:
+    print('Adding extra cheese.')
+
+print('\nFinished making your pizza!')
+
