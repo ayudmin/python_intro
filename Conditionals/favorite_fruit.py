8f209f0eb7ce36dc8b4dbc5e52eb5a5fc61644de
francis556 <vikrammehra244@gmail.com>
2023-07-15 15:49:16 +0200

    info: favorite fruits

---
+++ Conditionals/favorite_fruit.py
@@ -0,0 +1,8 @@
+favorite_fruits = ['apple', 'bananas', 'lemon', 'grapes']
+
+if 'apple' in favorite_fruits:
+    print('\nYou really like apples!')
+if 'bananas' in favorite_fruits:
+    print('\nYou really like bananas!')
+if 'grapes' in favorite_fruits:
+    print('\nYou really like grapes!')
