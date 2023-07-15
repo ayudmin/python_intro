89b090b8ccf11b7fd7bc938a1f7561b9a79d2cf8
francis556 <vikrammehra244@gmail.com>
2023-07-15 14:21:53 +0200

    info: stages_of_life

---
+++ Conditionals/stages_of_life.py
@@ -0,0 +1,14 @@
+person_age = 65
+
+if person_age < 2:
+    print('Baby')
+elif person_age < 4:
+    print('Toddler')
+elif person_age < 13:
+    print('Kid')
+elif person_age < 20:
+    print('Teenager')
+elif person_age < 65:
+    print('Adult')
+elif person_age >= 65:
+    print('elder')
