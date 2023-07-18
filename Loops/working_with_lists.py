669c5387f9c80904abdba622c8c0059637a55581
ayudmin <vikrammehra244@gmail.com>
2023-07-18 17:31:07 +0200

    info: looping through

---
+++ Loops/working_with_lists.py
@@ -0,0 +1,32 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick {magician.title()}. \n")
+
+print('Thank you everyone, that was a great show.')
+print('\n')
+
+pizza_names = ['french_frie', 'chicken_pizza', 'my_nyam_nyam']
+
+for pizza in pizza_names:
+    print(f"I like {pizza} pizza.")
+
+print('WOW, really like eating pizza for fun!')
+print('Favorite choice is my_nyam_nyam')
+print('I really love pizza!')
+
+print('\n')
+
+cats = ['lion', 'leopard', 'chetter']
+
+for cat in cats:
+    if cat == 'lion':
+        print(f"A {cat} is very fiearc")
+    if cat == 'leopard':
+        print(f"A {cat} is very slow")
+    if cat == 'chetter':
+        print(f"A {cat} is very fast")
+
+print('All are wild cats.')
+
