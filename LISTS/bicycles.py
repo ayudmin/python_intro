33d029eff55d368cf98ccd96f7a449b2f380585e
francis556 <vikrammehra244@gmail.com>
2023-07-20 13:35:36 +0200

    info: lists

---
+++ LISTS/bicycles.py
@@ -0,0 +1,42 @@
+bicycles = ['trek', 'cannondale', 'redline',  'specialized']
+print(bicycles)
+print(bicycles[0].title())
+print(bicycles[-1].upper())
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+print('\n')
+print(message)
+
+friends = ['ayume', 'ayudmin', 'vikram', 'golddiger', 'merci']
+
+print('\n')
+print(f"Hello, {friends[0].title()}")
+print(f"Hello, {friends[1].title()}")
+print(f"Hello, {friends[2].title()}")
+print(f"Hello, {friends[3].title()}")
+print(f"Hello, {friends[4].title()}")
+print('\n')
+
+
+assets = ['eoearn', 'junubpussy', 'codejunub', 'kulusawa', 'sspaynow']
+
+print('\n')
+print(f"I will become CEO of {assets[3]} in 2024")
+print(f"I will make money from {assets[2]} in 2025")
+print(f"I will market my apps using  {assets[0]} in 2024")
+print(f"I will become a millioniare running {assets[1]} in 2024")
+print(f"I will get my money via {assets[4]} in 2024")
+print('\n')
+
+
+motorcyles = ['honda', 'yamaha', 'suzuki']
+motorcyles[0] = 'ducati'
+
+print(motorcyles)
+motorcyles.append('honda')
+print(motorcyles)
+
+
+
+
+
