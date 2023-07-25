d04aaef065ced5747d05932457c9324d1487919f
ayudmin <vikrammehra244@gmail.com>
2023-07-25 20:06:33 +0200

    info: copy lists

---
+++ Lists/foods.py
@@ -0,0 +1,11 @@
+foods = ['pizza', 'falafel', 'carrot cake']
+
+new_foods = foods[:]
+foods.append('tomato')
+new_foods.append('lemon')
+foods.sort()
+print(foods)
+new_foods.reverse()
+print(new_foods)
+
+print('\n')
