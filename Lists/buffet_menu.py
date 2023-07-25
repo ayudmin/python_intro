89a40417d1af577e62c44094267ef32fe2a9ba89
ayudmin <vikrammehra244@gmail.com>
2023-07-25 20:45:20 +0200

    info: tuples

---
+++ Lists/buffet_menu.py
@@ -0,0 +1,10 @@
+menu = ('salad', 'chicken', 'fish', 'beef', 'vegetables')
+
+for item in menu:
+    print(item)
+
+print('\n')
+menu = ('salad', 'chicken', 'fish', 'rice', 'ice cream')
+
+for new_item in menu:
+    print(new_item)
---
+++ Lists/tuples.py
@@ -0,0 +1,11 @@
+dimensions = (200, 50)
+
+print('Orginal dimensions')
+for dimension in dimensions:
+    print(dimension)
+
+print('\n')
+print('New dimensions')
+dimensions = (400, 300)
+for dimension in dimensions:
+    print(dimension)
