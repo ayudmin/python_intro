3fc9b9404e6ad716beaf8e57897d6e573ae7f26e
ayudmin <vikrammehra244@gmail.com>
2023-07-25 19:18:57 +0200

    info: sort, reverse lists

---
+++ Lists/places.py
@@ -0,0 +1,18 @@
+places = ['Germany', 'Malaysia', 'South Africa', 'Senegal', 'England']
+
+print(places)
+print(sorted(places))
+print(sorted(places, reverse=True))
+print(places)
+print('\n')
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+print('\n')
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+
+print(len(places))
