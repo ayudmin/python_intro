d965c835a89dae076cd8b1a8393ad83796f52b98
francis556 <vikrammehra244@gmail.com>
2023-08-17 13:06:43 +0200

    info: classes

--- Classes/restaurant.py
+++ Classes/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"Restaurant name: {self.restaurant_name}")
@@ -10,13 +11,22 @@ class Restaurant:
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open.")
 
+    def set_number_served(self, number_of_customers):
+        self.number_served = number_of_customers
+
+    def increment_number_served(self, number_of_customers):
+        self.number_served += number_of_customers
+
 
 restaurant = Restaurant('Istanbul', 'Pizaa')
 print(restaurant.restaurant_name)
 print(restaurant.cuisine_type)
+restaurant.set_number_served(10)
+restaurant.increment_number_served(10)
+print(restaurant.number_served)
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
-second_restaurat = Restaurant('Mama Tabia', 'Mula Kombo')
-second_restaurat.describe_restaurant()
+second_restaurant = Restaurant('Mama Tabia', 'Mula Kombo')
+second_restaurant.describe_restaurant()
--- Classes/user.py
+++ Classes/user.py
@@ -4,22 +4,33 @@ class User:
         self.last_name = last_name
         self.age = age
         self.sex = sex
+        self.login_attempt = 0
 
     def describe_user(self):
-        user_profile = {}
-        user_profile['first_name'] =  self.first_name
-        user_profile[ 'last_name'] = self.last_name
-        user_profile['age'] = self.age
-        user_profile['sex'] = self.sex
+        user_profile = {'first_name': self.first_name, 'last_name': self.last_name, 'age': self.age, 'sex': self.sex}
         print(user_profile)
 
     def greet_user(self):
         print(f'Bonjour {self.first_name} {self.last_name}')
 
+    def increment_login_attempt(self):
+        self.login_attempt += 1
+
+    def reset_login_attempts(self):
+        self.login_attempt = 0
+
+
+print('\n')
 ayudmin = User('ayud', 'min', 23, 'male')
 ayudmin.describe_user()
+print(ayudmin.login_attempt
+)
+ayudmin.increment_login_attempt()
+ayudmin.increment_login_attempt()
+ayudmin.increment_login_attempt()
+ayudmin.increment_login_attempt()
+print(ayudmin.login_attempt
+)
+ayudmin.reset_login_attempts()
+print(ayudmin.login_attempt)
 ayudmin.greet_user()
-print('\n')
-user_new = User('ayume', 'francis', 23, 'male')
-user_new.describe_user()
-user_new.greet_user()
